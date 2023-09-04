1f265d6cb17518acdc23f662ce57837542904660
gdou2 <george.dou@teletracnavman.com>
2023-09-04 20:30:49 +1200

    format Unknown

--- src/main/java/com/example/calculator/operators/Unknown.java
+++ src/main/java/com/example/calculator/operators/Unknown.java
@@ -2,8 +2,7 @@ package com.example.calculator.operators;
 
 import com.example.calculator.operands.Operand;
 
-public
-class Unknown implements Operator {
+public class Unknown implements Operator {
     private String str;
 
     public Unknown(String str) {
